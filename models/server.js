41bef3e0e5740a4a1663468b0b1fea3223891f25
Fernando Nicolás <fersanti2896@gmail.com>
2023-08-10 01:23:14 -0600

    Implementación para generar JWT.

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
+        this.authPath = '/api/auth';
 
         /* Conexión a la base de datos */
         this.connectionDB();
@@ -31,6 +32,7 @@ class Server {
 
     routes() {
         this.app.use( this.usuariosPath, require('../routes/user') );
+        this.app.use( this.authPath, require('../routes/auth') );
     }
 
     listen() {
